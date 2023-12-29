cb24317be62eecd7417539edc4b3374422b41e8b
Ankit <theankitpurohit@gmail.com>
2023-12-29 13:36:47 +0530

    React Query added

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider as StoreProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from 'src/app';
 import { store, persistor } from 'src/store';
+
+// Create a client
+const queryClient = new QueryClient();
 
 // ----------------------------------------------------------------------
 
@@ -16,11 +21,14 @@ root.render(
   <StoreProvider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <HelmetProvider>
-        <BrowserRouter>
-          <Suspense>
-            <App />
-          </Suspense>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <Suspense>
+              <App />
+            </Suspense>
+          </BrowserRouter>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </HelmetProvider>
     </PersistGate>
   </StoreProvider>

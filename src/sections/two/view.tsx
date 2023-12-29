373c49b1541474ec618165bc7dc31f95082d0e8c
Harsh G <harshsoftcolon1105@gmail.com>
2023-12-29 17:52:44 +0530

    added mui dataGrid

--- src/sections/two/view.tsx
+++ src/sections/two/view.tsx
@@ -6,9 +6,18 @@ import Typography from '@mui/material/Typography';
 import { useSettingsContext } from 'src/components/settings';
 
 // ----------------------------------------------------------------------
+const AnyReactComponent = (text: any) => <div>{text}</div>;
 
 export default function TwoView() {
   const settings = useSettingsContext();
+
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627,
+    },
+    zoom: 11,
+  };
 
   return (
     <Container maxWidth={settings.themeStretch ? false : 'xl'}>
@@ -18,12 +27,21 @@ export default function TwoView() {
         sx={{
           mt: 5,
           width: 1,
-          height: 320,
+          p: '10px',
+          height: 600,
           borderRadius: 2,
           bgcolor: (theme) => alpha(theme.palette.grey[500], 0.04),
           border: (theme) => `dashed 1px ${theme.palette.divider}`,
         }}
-      />
+      >
+        {/* <GoogleMapReact
+            bootstrapURLKeys={{ key: '' }}
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}
+          >
+            <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+          </GoogleMapReact> */}
+      </Box>
     </Container>
   );
 }
